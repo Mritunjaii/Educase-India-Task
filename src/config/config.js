4dd103be86c96970db34a94ce11040f8633b6d87
Mritunjaii <themritunjai@gmail.com>
2025-05-28 09:56:43 +0530

    Refactor database configuration to use environment variables

--- src/config/config.js
+++ src/config/config.js
@@ -5,11 +5,11 @@ dotenv.config();
 module.exports = {
   BACKEND_PORT: process.env.BACKEND_PORT || 3001,
   development: {
-    username: 'root',
-    password: '12345678',
-    database: 'educasetask',
-    host: '127.0.0.1',
-    dialect: 'mysql'
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "mysql"
   },
   production: {
     username: process.env.DB_USER,
